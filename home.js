dc05d6e60b2d0b7a401a56eb351b695854232c08
Harmanjit Singh <156525248+harmanpannu1@users.noreply.github.com>
2024-11-30 16:19:05 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,56 @@
+let btn = document.querySelector(".btn_first");
+btn.addEventListener("click", () => {
+    window.location.href = "C:/Users/Hp/OneDrive/CSE326/Project/files/KnowMore_1.html";
+});
+
+let btnSubs = document.querySelector(".btn_subs");
+btnSubs.addEventListener( "click", () => {
+    window.location.href = "C:/Users/Hp/OneDrive/CSE326/Project/files/subsc.html";
+});
+
+
+let user = document.querySelector(".user");
+user.addEventListener("click", () => {
+    window.location.href = "C:/Users/Hp/OneDrive/CSE326/Project/files/form.html";
+});
+
+let bestseller = document.querySelector(".bestseller");
+bestseller.addEventListener("click", () => {
+    window.location.href = "C:/Users/Hp/OneDrive/CSE326/Project/order/bestseller.html";
+});
+
+let drinks = document.querySelector(".drinks");
+drinks.addEventListener("click", () => {
+    window.location.href = "C:/Users/Hp/OneDrive/CSE326/Project/order/drinks.html";
+});
+
+let food = document.querySelector(".food");
+food.addEventListener("click", () => {
+    window.location.href = "C:/Users/Hp/OneDrive/CSE326/Project/order/food.html";
+});
+
+let merchandise = document.querySelector(".merchandise");
+merchandise.addEventListener("click", () => {
+    window.location.href = "C:/Users/Hp/OneDrive/CSE326/Project/order/Merchandise.html";
+});
+
+let rte = document.querySelector(".rte");
+rte.addEventListener("click", () => {
+    window.location.href = "C:/Users/Hp/OneDrive/CSE326/Project/order/rte.html";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+let div = document.querySelector(".hello")
+div.addEventListener("click", () => {
+    window.location.href="knowMore.html";
+})
